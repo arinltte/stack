3cd554082fe63adfb24102b6c233829e14f13546
arinltte <166289099+arinltte@users.noreply.github.com>
2026-06-02 01:10:02 +0800

    Initial Commit

---
+++ stack/stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackApp.swift
+//  stack
+//
+//  Created by Chen Jin Shen on 02/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
